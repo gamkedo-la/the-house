0fa65cdf99190b08e1f28ad89a3820b0b555ca68
Joël Lamotte (Klaim) <mjklaim@gmail.com>
2022-09-04 17:19:12 +0200

    Added text displaying list of keys (for debug)

---
+++ player/debug_info.gd
@@ -0,0 +1,21 @@
+extends RichTextLabel
+
+onready var _info = $"../debug_info"
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	if Input.is_action_just_pressed("debug_info"):
+		toggle_debug_info()
+
+func toggle_debug_info():
+	if visible:
+		hide()
+		_info.show()
+	else:
+		_info.hide()
+		show()

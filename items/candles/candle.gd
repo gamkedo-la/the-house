ad6590adf6f11540e2061163c94848c47bdcc8e3
Joël Lamotte (Klaim) <mjklaim@gmail.com>
2022-09-18 18:39:15 +0200

    Candles can be lit off by using them in hand.

--- items/candles/candle.gd
+++ items/candles/candle.gd
@@ -2,11 +2,6 @@ extends InteractiveItem
 
 onready var light : Node = $"light"
 
-
-func _on_lightable_area_lit_using_fire() -> void:
-	print("time to turn on through fire")
-	light_on()
-
 func light_on() -> void:
 	print("turning light on")
 	light.show()
@@ -16,3 +11,14 @@ func light_off() -> void:
 	light.hide()
 	
 # TODO: add ways to make the candle turn off when mishandled
+
+
+
+func _on_lightable_area_lit_using_fire() -> void:
+	print("time to turn on through fire")
+	light_on()
+
+
+func _on_Candle_use_item():
+	light_off()
+	

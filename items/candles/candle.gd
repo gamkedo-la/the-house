61f0531291a1a4579bb2cec55e8a7ae5aef64bee
Joël Lamotte (Klaim) <mjklaim@gmail.com>
2022-09-18 17:19:48 +0200

    Candle starts without light by default, and other tweaks

--- items/candles/candle.gd
+++ items/candles/candle.gd
@@ -1,16 +1,9 @@
 extends InteractiveItem
 
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
+onready var interraction_area := $"interraction_area"
 
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+

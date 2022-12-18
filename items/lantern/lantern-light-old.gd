80b7148f8509879cd537410db2d61982935f8cba
Joël Lamotte (Klaim) <mjklaim@gmail.com>
2022-12-18 18:51:52 +0100

    Now we have an old-style lantern and a modern lantern - behavior not changed yet.

---
+++ items/lantern/lantern-light-old.gd
@@ -0,0 +1,22 @@
+extends Spatial
+
+
+var is_on = false
+onready var light_source : OmniLight = $"./light"
+
+func toggle_light():
+	if is_on:
+		turn_off()
+	else:
+		turn_on()
+
+func turn_on():
+	is_on = true
+	light_source.visible = true
+	
+func turn_off():
+	is_on = false
+	light_source.visible = false
+
+func _on_lantern_use_item():
+	toggle_light()

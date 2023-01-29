2134f3d74451f29c64e865c526b11ce4d0fce806
Joël Lamotte (Klaim) <mjklaim@gmail.com>
2023-01-29 21:46:05 +0100

    Fixed shadow being very big + bobbing when walking (with bug which depends on which screen you run thing game......????????)

--- player/shadow_mesh.gd
+++ player/shadow_mesh.gd
@@ -1,23 +1,40 @@
 extends MeshInstance
 
 export var bob_size : float = 0.1
-export var bob_freq : float = 4
+export var bob_freq : float = 3.0
+export var bob_stop_speed : float = 0.5
 export var breathing_bob_size : float = 0.01
 export var breathing_bob_freq : float = 2.313
 
-func _process(_delta):
+onready var _last_position := _current_position_on_plane()
+onready var _moving_distance : float = 0.0
+onready var _origin_height : float = self.transform.origin.y
 
-	var time_since_beginning = OS.get_ticks_msec() * 0.001
-	var pos = global_transform.origin
-	var dist = pos.length()
+func _current_position_on_plane() -> Vector2:
+	var player : Spatial = get_parent()
+	return Vector2(player.global_transform.origin.x, player.global_transform.origin.z)
 
+func _process(delta):
+	
+	var height_modulation = 0.0
+		
 	# breathing constantly up and down
-	translation.y = sin(time_since_beginning * breathing_bob_freq) * breathing_bob_size
+	var time_since_beginning = Time.get_ticks_msec() * 0.001
+	height_modulation += sin(time_since_beginning * breathing_bob_freq) * breathing_bob_size
 
 	# bob the child mesh up and down based on pos (fake footstep animation!)
+	var new_pos = _current_position_on_plane()
+	var distance_since_last_pos = (new_pos - _last_position).length()
+
+	if distance_since_last_pos == 0.0:
+		_moving_distance = 0
+	else:
+		_moving_distance += distance_since_last_pos 
+	_moving_distance = max(_moving_distance, 0.0)
 	
-	# fixme: if you move diagonally the two sin waves can cancel out and no bob
-	# translation.y += sin((pos.x*bob_freq+pos.z*bob_freq)) * bob_size
+	height_modulation += sin(_moving_distance * bob_freq) * bob_size
+	_last_position = new_pos
 	
-	# fixme: if you circle strafe 0,0,0 you don't step (dist stays same)
-	translation.y += sin(dist * bob_freq) * bob_size
+	# apply height modulation
+	transform.origin.y = _origin_height + height_modulation
+	

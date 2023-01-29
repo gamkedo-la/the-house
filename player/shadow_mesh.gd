245857d36485b77214403b085c722f3f398c8780
Joël Lamotte (Klaim) <mjklaim@gmail.com>
2023-01-29 20:54:21 +0100

    Fixed breathing shadow bobbing

--- player/shadow_mesh.gd
+++ player/shadow_mesh.gd
@@ -1,25 +1,23 @@
 extends MeshInstance
 
-var bob_size = 0.1
-var bob_freq = 4
-var breathing_bob_size = 0.05
-var breathing_bob_freq = 1
-var age = 0
+export var bob_size : float = 0.1
+export var bob_freq : float = 4
+export var breathing_bob_size : float = 0.01
+export var breathing_bob_freq : float = 2.313
 
+func _process(_delta):
 
-func _process(delta):
-
-	age += delta
+	var time_since_beginning = OS.get_ticks_msec() * 0.001
 	var pos = global_transform.origin
 	var dist = pos.length()
 
 	# breathing constantly up and down
-	translation.y = sin(age*breathing_bob_freq)*breathing_bob_size
+	translation.y = sin(time_since_beginning * breathing_bob_freq) * breathing_bob_size
 
 	# bob the child mesh up and down based on pos (fake footstep animation!)
 	
 	# fixme: if you move diagonally the two sin waves can cancel out and no bob
-	# translation.z += sin((pos.x*bob_freq+pos.z*bob_freq)) * bob_size
+	# translation.y += sin((pos.x*bob_freq+pos.z*bob_freq)) * bob_size
 	
 	# fixme: if you circle strafe 0,0,0 you don't step (dist stays same)
-	translation.y += sin(dist*bob_freq) * bob_size
+	translation.y += sin(dist * bob_freq) * bob_size

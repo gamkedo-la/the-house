7afff6eb42d439e0816b49f8d40119bc238845a1
Joël Lamotte (Klaim) <mjklaim@gmail.com>
2023-02-03 22:32:12 +0100

    Added a fully functional crystal ball

---
+++ items/crystal_ball/cristal_ball.gd
@@ -0,0 +1,27 @@
+extends InteractiveItem
+
+onready var _light_node : OmniLight = $"light"
+onready var _sphere_node : CSGSphere = $"sphere"
+
+func _ready():
+	turn_off()
+
+func is_lighting() -> bool:
+	return _light_node.visible
+
+func turn_on() -> void:
+	_sphere_node.material.emission_enabled = true
+	_light_node.visible = true
+	
+func turn_off() -> void:
+	_sphere_node.material.emission_enabled = false
+	_light_node.visible = false
+	
+func switch_light() -> void:
+	if is_lighting():
+		turn_off()
+	else:
+		turn_on()
+
+func _on_crystal_ball_use_item() -> void:
+	switch_light()

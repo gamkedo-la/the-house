4a778e2f8d7e9ff53806e3052ab8be320e5f9780
Joël Lamotte (Klaim) <mjklaim@gmail.com>
2023-02-12 21:13:34 +0100

    Added foot (cut) placeholder

--- items/body/hand.gd
+++ items/body/hand.gd
@@ -1,5 +1,5 @@
 tool
-extends RigidBody
+extends InteractiveItem
 
 
 export var is_left_handed : bool = false setget _set_left_handed,_is_left_handed
@@ -7,7 +7,9 @@ export var is_left_handed : bool = false setget _set_left_handed,_is_left_handed
 
 func _set_left_handed(new_value: bool) -> void:
 	if is_left_handed != new_value:
-		scale = scale * Vector3(-1.0, 1.0, 1.0)
+		for child_node in get_children():
+			if child_node is Spatial:
+				child_node.scale = child_node.scale * Vector3(-1.0, 1.0, 1.0)
 	is_left_handed = new_value
 	
 func _is_left_handed() -> bool:

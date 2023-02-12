564320d75a252a89cf122508bfc1c661aca35575
Joël Lamotte (Klaim) <mjklaim@gmail.com>
2023-02-12 18:02:50 +0100

    Moved more environment stuffs into environment scene to fix some node access errors.

--- player/scene_cam.gd
+++ player/scene_cam.gd
@@ -1,23 +1,2 @@
 extends Camera
 
-onready var _pixelator : CanvasLayer = $"screen pixelation"
-onready var _airdust : CPUParticles = $"airdust_particles"
-onready var _environment : WorldEnvironment= $"%WorldEnvironment"
-
-
-func _process(delta):
-	# TEMPORARY:
-	if Input.is_action_just_pressed("debug_switch_pixelator"):
-		_pixelator.visible = not _pixelator.visible
-		print("screen pixelation: ", _pixelator.visible)
-	if Input.is_action_just_pressed("debug_switch_airdust"):
-		_airdust.visible = not _airdust.visible
-		print("air dust: ", _airdust.visible)
-	if _environment:
-		if Input.is_action_just_pressed("debug_switch_fog"):
-			_environment.environment.fog_enabled = not _environment.environment.fog_enabled
-			print("fog:  ", _environment.environment.fog_enabled)
-		if Input.is_action_just_pressed("debug_switch_glow"):
-			_environment.environment.glow_enabled = not _environment.environment.glow_enabled
-			print("glow:  ", _environment.environment.glow_enabled)
-	# END OF TEMPORARY

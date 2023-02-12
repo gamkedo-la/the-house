b17d163d9f2e117a11064230034844d0a38b1fe7
Joël Lamotte (Klaim) <mjklaim@gmail.com>
2023-02-12 17:44:34 +0100

    fixed issue from previous change

--- gameplay/key_area.gd
+++ gameplay/key_area.gd
@@ -7,7 +7,7 @@ class_name KeyArea
 # Key name, must match the name of the lock area
 export(String) var key_name : String
 
-onready var item : Node = CollisionLayers.get_item_parent_node(get_parent())
+onready var item := ItemUtils.get_item_parent_node(get_parent())
 
 func _ready():
 	set_collision_layer_bit(CollisionLayers.key_lock_collision_bit, true)

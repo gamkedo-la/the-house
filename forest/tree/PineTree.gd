5607507d25f2b3c0e53c1cd21b18e5345b102252
Joël Lamotte (Klaim) <mjklaim@gmail.com>
2023-02-18 22:24:31 +0100

    Fixed landscape slopes and tweaked trees (unsuccessfully)

---
+++ forest/tree/PineTree.gd
@@ -0,0 +1,22 @@
+tool
+extends Spatial
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	for child_mesh in get_children():
+		if child_mesh is MeshInstance:
+			child_mesh.mesh = child_mesh.mesh.duplicate(true)
+			for surface_idx in child_mesh.mesh.get_surface_count():
+				var material = child_mesh.mesh.surface_get_material(surface_idx)
+				if material is ShaderMaterial:
+					var offset = rand_range(0.5, 2.0)
+					print("setting tree material offset = ", offset)
+					material.set_shader_param("SwayOffset", offset)
+					
+

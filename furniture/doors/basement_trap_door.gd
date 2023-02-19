48e2f96e01bb5933a9564588f2e94aa395237296
Joël Lamotte (Klaim) <mjklaim@gmail.com>
2023-02-19 19:25:42 +0100

    Fixed (but not convinced it's right) crash when opening the basement trap door.

--- furniture/doors/basement_trap_door.gd
+++ furniture/doors/basement_trap_door.gd
@@ -5,6 +5,6 @@ func _ready() -> void:
 	connect("door_opened", self, "_on_door_opened")
 	
 func _on_door_opened()-> void:
-	$"%basement_ladder".visible = true
+	$"../../basement_ladder".visible = true
 
  

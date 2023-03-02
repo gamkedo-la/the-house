bdeabdd4672b54b25cc3a3072c5611c1bd74a37f
Joël Lamotte (Klaim) <mjklaim@gmail.com>
2023-03-02 18:14:53 +0100

    Fix the resolution only in HTML5, let Desktop do as they wish

--- main.gd
+++ main.gd
@@ -2,6 +2,9 @@ extends Node
 
 	
 func _ready():
+	if OS.get_name() == "HTML5":
+		get_tree().set_screen_stretch(SceneTree.STRETCH_MODE_VIEWPORT, SceneTree.STRETCH_ASPECT_KEEP, Vector2(1920, 1080))
+	
 	if name == "main":
 		to_title_screen()
 	

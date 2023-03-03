c6e8b37cba080408f58024ec7c09af955f06453b
Joël Lamotte (Klaim) <mjklaim@gmail.com>
2023-03-03 04:48:04 +0100

    Improved title screen's loading display

--- screens/title_screen.gd
+++ screens/title_screen.gd
@@ -2,6 +2,10 @@ extends Spatial
 
 var master_scene = null # Dont use this except if this is a scene below main when the game is normally running
 
+onready var _main_menu : CanvasLayer = $"%main_menu"
+onready var _options_screen : Node2D = $"%options_screen"
+onready var _credits_screen : Node2D = $"%credits_screen"
+onready var _loading : CanvasLayer =  $"%loading"
 
 func _ready():
 	$"%start_button".connect("pressed", self, "_start_new_game")
@@ -9,4 +13,7 @@ func _ready():
 
 func _start_new_game():
 	if master_scene:
+		_main_menu.visible = false
+		_loading.visible = true
+		yield(get_tree().create_timer(0.1), "timeout")
 		master_scene.start_new_game()

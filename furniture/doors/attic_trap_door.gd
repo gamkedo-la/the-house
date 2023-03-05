3bbb63cad5b1b9ec412c7390212df5aca58dd7a0
Joël Lamotte (Klaim) <mjklaim@gmail.com>
2023-03-05 15:54:00 +0100

    Fixed ladders being active when they are invisible

--- furniture/doors/attic_trap_door.gd
+++ furniture/doors/attic_trap_door.gd
@@ -1,11 +1,18 @@
 tool
 extends Door
 
-
 func _ready() -> void:
 	connect("door_unlocked", self, "_on_door_unlocked")
+	connect("door_locked", self, "_on_door_locked")
+	lock()
 	
 func _on_door_unlocked()-> void:
-	$"../../attic_ladder".visible = true
+	var ladder = $"../../attic_ladder"
+	if ladder:
+		ladder.visible = true
 
+func _on_door_locked()-> void:
+	var ladder = $"../../attic_ladder"
+	if ladder:
+		ladder.visible = false
  
--- test_scenes/test-ladder.gd
+++ test_scenes/test-ladder.gd
@@ -1,11 +1,13 @@
 extends StaticBody
 
 onready var _climbing_area = $"climbing_area"
+onready var _collision_shape = $"CollisionShape"
 
 func _ready():
 	connect("visibility_changed", self, "_update_climbing_area")
 	_update_climbing_area()
 
 func _update_climbing_area() -> void:
-	_climbing_area.set_deferred("monitoring", self.visible)
-#	_climbing_area.set_deferred("monitorable", self.visible)
+	_climbing_area.monitoring = self.visible
+	_climbing_area.monitorable = self.visible
+	_collision_shape.disabled = not self.visible

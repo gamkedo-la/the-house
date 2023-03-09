c70e5a65a26bc9dd1edb9572afa19953b891688d
Joël Lamotte (Klaim) <mjklaim@gmail.com>
2023-03-09 01:22:58 +0100

    Completed the key/lock with basement

--- furniture/doors/basement_trap_door.gd
+++ furniture/doors/basement_trap_door.gd
@@ -4,8 +4,11 @@ extends Door
 
 func _ready() -> void:
 	connect("door_opened", self, "_on_door_opened")
+	find_node("lock").connect("unlocked", self, "_on_lock_unlocked")
 	
 func _on_door_opened()-> void:
 	$"../../basement_ladder".visible = true
 
+func _on_lock_unlocked(_keyname) -> void:
+	unlock()
  

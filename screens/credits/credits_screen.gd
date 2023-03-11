af1c210bf8b5883e6174b6f083c6f31bc3b8a209
Joël Lamotte (Klaim) <mjklaim@gmail.com>
2023-03-11 01:59:03 +0100

    Improved credits screen and set it up to be ready to be filled with real content.

--- screens/credits/credits_screen.gd
+++ screens/credits/credits_screen.gd
@@ -3,8 +3,16 @@ extends Node2D
 var master_scene = null
 var scene_name_to_get_back_to : String = "title_screen"
 
+const text_file_path := "res://screens/credits/credits_text.txt" # BEWARE: THIS FILE/PATH MUST BE MANUALLY SET AS BEING EXPORTED IN EACH EXPORT CONFIGURATION
+
 func _ready():
-	$back_button.connect("pressed", self, "_on_back_pressed")
+	var file = File.new()
+	file.open(text_file_path, File.READ)
+	var credits_text = file.get_as_text()
+	file.close()
+	
+	$"%credits_text".bbcode_text = credits_text
+	$"%back_button".connect("pressed", self, "_on_back_pressed")
 
 
 func _on_back_pressed():

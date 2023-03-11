9c3a96f667db774091a683f5a911d4d1b5ea81c9
Joël Lamotte (Klaim) <mjklaim@gmail.com>
2023-03-12 00:53:08 +0100

    Completed opening the bedroom door with the music box (using music placeholder for now)

--- furniture/bedroom_door.gd
+++ furniture/bedroom_door.gd
@@ -1,4 +1,35 @@
 tool
 extends Door
 
+onready var _listening_area = $listening_area
 
+var _audio_source
+var _is_listening := true
+
+func _ready():
+	_listening_area.set_collision_mask_bit(CollisionLayers.audio_lock_collision_bit, true)
+	_listening_area.connect("body_entered", self, "_on_audio_source_entered")
+	_listening_area.connect("body_exited", self, "_on_audio_source_exited")
+	
+func _on_audio_source_entered(source) -> void:
+	if source is MusicBox:
+		print("music box is close to bedroom door")
+		_audio_source = source
+
+func _on_audio_source_exited(source) -> void:
+	if source is MusicBox:
+		_audio_source = null
+		print("music box is far to bedroom door")
+
+func _process(_delta):
+	if _is_listening and _audio_source is MusicBox and _audio_source.is_playing_music:
+		_unlock_the_door()
+
+func _unlock_the_door():
+	print("listening door unlocked")
+	_is_listening = false
+	_listening_area.set_deferred("monitoring", false)
+	_listening_area.set_deferred("monitorable", false)
+	
+	unlock()
+	
--- game/collisions_layers.gd
+++ game/collisions_layers.gd
@@ -8,3 +8,4 @@ const climbing_area_collision_bit := 2
 const player_interraction_raycast_layer_bit := 7
 const key_lock_collision_bit := 9
 const lighting_by_fire_collision_bit := 10
+const audio_lock_collision_bit := 11

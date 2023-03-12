ad927465ff1602fc24a0ceb56d2e3deb9cd3829a
Joël Lamotte (Klaim) <mjklaim@gmail.com>
2023-03-12 18:16:17 +0100

    Locked the chest and screwed  human feet on it's lid }:D

--- items/treasure_chest/treasure_chest.gd
+++ items/treasure_chest/treasure_chest.gd
@@ -2,6 +2,7 @@ tool
 extends StaticBody
 
 export var is_open := false setget _set_open_chest
+export var is_locked := true
 
 onready var _animations : AnimationPlayer = $AnimationPlayer
 
@@ -12,10 +13,23 @@ func _ready():
 		_set_open_chest(is_open)
 
 func player_interracts():
+	if is_locked:
+		_display_locked_description()
+		return
+		
 	if is_open:
 		close_chest()
 	else:
 		open_chest()
+
+func _display_locked_description() -> void:
+	global.current_player.examination_display.display_text_sequence([
+		"This chest is locked. I need [b]a code[/b] to open it...\nThe human [b]feet[/b] screwed on it's lid seems to be part of a mechanism..."
+	])
+	
+func on_player_end_pointing() -> void:
+	if is_locked:
+		global.current_player.examination_display.stop_display_sequence()
 
 func open_chest() -> void:
 	_animations.play("lid-opening")

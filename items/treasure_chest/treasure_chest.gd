ccdd61f7ad41ec1bf426f80cc78b1f4846e0830e
Joël Lamotte (Klaim) <mjklaim@gmail.com>
2023-03-12 15:41:25 +0100

    Treasure chest can be open and closed in the Editor.

--- items/treasure_chest/treasure_chest.gd
+++ items/treasure_chest/treasure_chest.gd
@@ -1,6 +1,7 @@
+tool
 extends StaticBody
 
-export var is_open := false
+export var is_open := false setget _set_open_chest
 
 onready var _animations : AnimationPlayer = $AnimationPlayer
 
@@ -17,3 +18,16 @@ func open_chest() -> void:
 	_animations.play("lid-opening")
 	is_open = true
 
+func close_chest() -> void:
+	_animations.play("default")
+	is_open = false
+
+func _set_open_chest(new_value : bool) -> void:
+	if new_value == is_open:
+		return
+		
+	if new_value:
+		open_chest()
+	else:
+		close_chest()
+		

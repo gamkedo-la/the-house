c32172f3c23669a716e128d7a6a654e1ae3b59d7
Joël Lamotte (Klaim) <mjklaim@gmail.com>
2023-03-15 02:03:02 +0100

    basement trap door removes the ladder when closed and dont have a way to close itself when in the basement

--- furniture/doors/basement_trap_door.gd
+++ furniture/doors/basement_trap_door.gd
@@ -4,10 +4,14 @@ extends Door
 
 func _ready() -> void:
 	connect("door_opened", self, "_on_door_opened")
+	connect("door_closed", self, "_on_door_closed")
 	find_node("lock").connect("unlocked", self, "_on_lock_unlocked")
 	
 func _on_door_opened()-> void:
 	$"../../basement_ladder".visible = true
+	
+func _on_door_closed()-> void:
+	$"../../basement_ladder".visible = false
 
 func _on_lock_unlocked(_keyname) -> void:
 	unlock()

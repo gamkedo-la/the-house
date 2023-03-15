ebfd3df5a898b086b5c516f528f137df6e0db39c
Joël Lamotte (Klaim) <mjklaim@gmail.com>
2023-03-15 23:08:33 +0100

    Tweaks to the narration

--- game/take_instructions_text.gd
+++ game/take_instructions_text.gd
@@ -2,7 +2,9 @@ extends TextDisplayArea
 
 
 func _process(_delta):
-	if _player is Player and _player.is_holding_item():
-		monitoring = false
-		$"../examine_instructions".monitoring = true
+	if _player is Player:
+		var item = _player.get_item_in_hand()
+		if item is Mushroom and item.mushroom_color == Mushroom.MushroomColor.yellow:
+			monitoring = false
+			$"../examine_instructions".monitoring = true
 

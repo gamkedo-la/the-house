a15d78cb137945635734da7fecf921566b7a93e4
Joël Lamotte (Klaim) <mjklaim@gmail.com>
2023-03-17 00:06:40 +0100

    Converted house walls into collision boxes - WIP

---
+++ test_scenes/test_csgbox_to_collisionshape.gd
@@ -0,0 +1,17 @@
+tool
+extends EditorScript
+
+func _run() -> void:
+	_add_collision_box_from_csgbox(get_scene())
+	
+
+func _add_collision_box_from_csgbox(node: Node) -> void:
+	if node is CSGBox:
+		var collision_box = box_conversions.convert_csg_box_to_collision_box(node)
+		get_scene().add_child(collision_box)
+		collision_box.set_owner(get_scene())
+		print("new collision box : %s" % collision_box.name)
+		
+	for child in node.get_children():
+		_add_collision_box_from_csgbox(child)
+		

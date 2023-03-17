b5e18c8516766bb7d268bcff5f857ce754e77257
Joël Lamotte (Klaim) <mjklaim@gmail.com>
2023-03-17 16:37:13 +0100

    Deactivate more dev tools

--- game/game-environment.gd
+++ game/game-environment.gd
@@ -9,18 +9,19 @@ func _ready():
 	$"%WorldEnvironment".environment.background_sky.set_panorama(iChannel)
 
 func _process(_delta):
-	# TEMPORARY:
-	if Input.is_action_just_pressed("debug_switch_pixelator"):
-		_pixelator.visible = not _pixelator.visible
-		print("screen pixelation: ", _pixelator.visible)
-	if Input.is_action_just_pressed("debug_switch_airdust"):
-		_airdust.visible = not _airdust.visible
-		print("air dust: ", _airdust.visible)
-	if _environment:
-		if Input.is_action_just_pressed("debug_switch_fog"):
-			_environment.environment.fog_enabled = not _environment.environment.fog_enabled
-			print("fog:  ", _environment.environment.fog_enabled)
-		if Input.is_action_just_pressed("debug_switch_glow"):
-			_environment.environment.glow_enabled = not _environment.environment.glow_enabled
-			print("glow:  ", _environment.environment.glow_enabled)
-	# END OF TEMPORARY
+	
+	if global.is_dev_mode:
+		if Input.is_action_just_pressed("debug_switch_pixelator"):
+			_pixelator.visible = not _pixelator.visible
+			print("screen pixelation: ", _pixelator.visible)
+		if Input.is_action_just_pressed("debug_switch_airdust"):
+			_airdust.visible = not _airdust.visible
+			print("air dust: ", _airdust.visible)
+		if _environment:
+			if Input.is_action_just_pressed("debug_switch_fog"):
+				_environment.environment.fog_enabled = not _environment.environment.fog_enabled
+				print("fog:  ", _environment.environment.fog_enabled)
+			if Input.is_action_just_pressed("debug_switch_glow"):
+				_environment.environment.glow_enabled = not _environment.environment.glow_enabled
+				print("glow:  ", _environment.environment.glow_enabled)
+		

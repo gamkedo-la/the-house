89aa174715f12a93663b22d3d49c848cb344ab66
Joël Lamotte (Klaim) <mjklaim@gmail.com>
2023-03-19 20:12:32 +0100

    Toolshed toor now clarifies it's not a human hand you need...

---
+++ furniture/doors/door_handle_handshake.gd
@@ -0,0 +1,15 @@
+extends "res://furniture/doors/door_handle.gd"
+
+onready var _lock : Area = $lock
+
+func _ready() -> void:
+	_lock.connect("body_entered", self, "_on_hand_entered")
+	_lock.connect("body_exited", self, "_on_hand_removed")
+
+func _on_hand_entered(hand) -> void:
+	if hand is BodyHand:
+		global.current_player.examination_display.display_text_sequence(["This hand doesnt seem to fit that handle...I think it needs to be more solid."])
+		
+func _on_hand_removed(hand) -> void:
+	if hand is BodyHand:
+		global.current_player.examination_display.stop_display_sequence()

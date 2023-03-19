caf09ac68d1aa337ab6d377bbf2f908a61d008e4
Joël Lamotte (Klaim) <mjklaim@gmail.com>
2023-03-19 20:53:09 +0100

    Display the not-the-right-hand text only if it's not the golden hand

--- furniture/doors/door_handle_handshake.gd
+++ furniture/doors/door_handle_handshake.gd
@@ -7,9 +7,9 @@ func _ready() -> void:
 	_lock.connect("body_exited", self, "_on_hand_removed")
 
 func _on_hand_entered(hand) -> void:
-	if hand is BodyHand:
+	if hand is BodyHand and not hand is GoldenHand:
 		global.current_player.examination_display.display_text_sequence(["This hand doesnt seem to fit that handle...I think it needs to be more solid."])
 		
 func _on_hand_removed(hand) -> void:
-	if hand is BodyHand:
+	if hand is BodyHand and not hand is GoldenHand:
 		global.current_player.examination_display.stop_display_sequence()

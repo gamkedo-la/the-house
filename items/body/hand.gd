b43b0a400fc2a41b022c31818d76d7676a2d443f
Joël Lamotte (Klaim) <mjklaim@gmail.com>
2023-03-31 03:16:55 +0200

    Fixed wrong axis when flipping body parts to handle left/right versions.

--- items/body/hand.gd
+++ items/body/hand.gd
@@ -10,7 +10,7 @@ func _set_left_handed(new_value: bool) -> void:
 	if is_left_handed != new_value:
 		for child_node in get_children():
 			if child_node is Spatial:
-				child_node.scale = child_node.scale * Vector3(-1.0, 1.0, 1.0)
+				child_node.scale = child_node.scale * Vector3(1.0, 1.0, -1.0)
 	is_left_handed = new_value
 	
 func _is_left_handed() -> bool:

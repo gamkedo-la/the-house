5585e5b147a8879b24bc2267f53c3f82f81bc205
Joël Lamotte (Klaim) <mjklaim@gmail.com>
2023-04-02 20:49:37 +0200

    Graphics options: all implemented

--- code-library/Autoloads/options.gd
+++ code-library/Autoloads/options.gd
@@ -6,3 +6,26 @@ var toggle_hold := false
 
 var mouse_camera_speed := 1.0
 
+onready var fullscreen := false setget _set_fullscreen
+onready var vsync := true setget _set_vsync
+var fov := 70.0
+
+var pixelated := false
+
+# TODO: persist these options and apply what's necessary in _ready()
+func _ready() -> void:
+	if OS.get_name() == "HTML5":
+		get_tree().set_screen_stretch(SceneTree.STRETCH_MODE_VIEWPORT, SceneTree.STRETCH_ASPECT_KEEP, Vector2(1920, 1080))
+	
+	if OS.has_feature("standalone"): # If we didnt run it from the editor
+		_set_fullscreen(true)
+		
+	
+func _set_fullscreen(value: bool) -> void:
+	if OS.get_name() != "HTML5":
+		fullscreen = value
+		OS.window_fullscreen = value
+
+func _set_vsync(value: bool) -> void:
+	vsync = value
+	OS.vsync_enabled = value
--- player/scene_cam.gd
+++ player/scene_cam.gd
@@ -1,2 +1,5 @@
 extends Camera
 
+func _process(_delta):
+	if options.fov != self.fov:
+		self.fov = options.fov

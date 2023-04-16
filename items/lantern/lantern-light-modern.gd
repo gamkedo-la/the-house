a2ca97c9b5066d4c61cd3b3d140fa92e82ea2a39
Joël Lamotte (Klaim) <mjklaim@gmail.com>
2023-04-16 18:25:16 +0200

    Added lanterns on/off sounds

--- items/lantern/lantern-light-modern.gd
+++ items/lantern/lantern-light-modern.gd
@@ -4,6 +4,13 @@ extends Spatial
 var is_on = false
 onready var light_source : OmniLight = $"./light"
 
+onready var audio_player := $"%audio_player"
+
+onready var _sound_on : AudioStream = load("res://items/torchlight/on.wav")
+onready var _sound_off : AudioStream = load("res://items/torchlight/off.wav")
+
+var _is_ready := false
+
 func toggle_light():
 	if is_on:
 		turn_off()
@@ -14,9 +21,15 @@ func turn_on():
 	is_on = true
 	light_source.visible = true
 
+	audio_player.stream = _sound_on
+	audio_player.play()
+
 func turn_off():
 	is_on = false
 	light_source.visible = false
+
+	audio_player.stream = _sound_off
+	audio_player.play()
 
 func _on_lantern_use_item(_lantern):
 	toggle_light()
--- items/lantern/lantern-light-old.gd
+++ items/lantern/lantern-light-old.gd
@@ -3,14 +3,21 @@ extends Spatial
 
 var is_on = false
 onready var light_source := $"./light_source"
+onready var audio_player := $"%audio_player"
+
+onready var _sound_off : AudioStream = load("res://items/torchlight/off.wav")
 
 func turn_on():
 	is_on = true
 	light_source.visible = true
 
+
 func turn_off():
 	is_on = false
 	light_source.visible = false
+
+	audio_player.stream = _sound_off
+	audio_player.play()
 
 func _on_lantern_use_item(_lantern):
 	turn_off()

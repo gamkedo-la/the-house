6928338109b719fc54a483d48093d2e2c2018463
Joël Lamotte (Klaim) <mjklaim@gmail.com>
2023-04-16 18:12:21 +0200

    Added crystal ball sounds

--- items/crystal_ball/cristal_ball.gd
+++ items/crystal_ball/cristal_ball.gd
@@ -2,9 +2,18 @@ extends InteractiveItem
 
 onready var _light_node : OmniLight = $"light"
 onready var _sphere_node : MeshInstance = $"sphere"
+onready var _audio_player := $audio_player
+
+onready var _sound_on : AudioStream = load("res://audio/sounds/crystal-ball-on.mp3")
+onready var _sound_off : AudioStream = load("res://audio/sounds/crystal-ball-off.mp3")
+
+var _is_ready := false
 
 func _ready():
 	turn_off()
+
+	_is_ready = true
+
 
 func is_lighting() -> bool:
 	return _light_node.visible
@@ -13,9 +22,17 @@ func turn_on() -> void:
 	_sphere_node.get_active_material(0).emission_enabled = true
 	_light_node.visible = true
 
+	if _is_ready:
+		_audio_player.stream = _sound_on
+		_audio_player.play()
+
 func turn_off() -> void:
 	_sphere_node.get_active_material(0).emission_enabled = false
 	_light_node.visible = false
+
+	if _is_ready:
+		_audio_player.stream = _sound_off
+		_audio_player.play()
 
 func switch_light() -> void:
 	if is_lighting():

ec72e40750670d55ef135b0b05ec7821fd629943
Joël Lamotte (Klaim) <mjklaim@gmail.com>
2023-04-23 17:29:04 +0200

    Fixed: zoom not working in examination mode

--- player/scene_cam.gd
+++ player/scene_cam.gd
@@ -1,5 +1,5 @@
 extends Camera
 
 func _process(_delta):
-	if options.fov != self.fov:
+	if options.fov != self.fov and (not global.current_player or not global.current_player.is_examining()):
 		self.fov = options.fov
